838ce8c7eb3cb83022b5f4493c13c0e7a26e2def
Daniel Oltmanns <53529846+oltdaniel@users.noreply.github.com>
2020-08-03 14:47:23 +0200

    fix agent summary navigation + added popper.js for bootstrap

--- vendor/assets/javascripts/bootstrap.js
+++ vendor/assets/javascripts/bootstrap.js
@@ -1,3 +1,4 @@
+//= require popper
 //= require bootstrap/util
 //= require bootstrap/alert
 //= require bootstrap/button
@@ -11,3 +12,19 @@
 //= require bootstrap/scrollspy
 //= require bootstrap/tab
 //= require bootstrap/toast
+
+/* This fix by https://github.com/twbs/bootstrap/issues/24870
+ * The issue: This project uses jQuery 4+, however the new bootstrap
+ * only works with an older version.
+ */
+$.prototype.tooltip = (function (tooltip) {
+  return function (config) {
+    try {
+      return tooltip.call(this, config);
+    } catch (ex) {
+      if (ex instanceof TypeError && config === "destroy") {
+        return tooltip.call(this, "dispose");
+      }
+    }
+  };
+})($.prototype.tooltip);
